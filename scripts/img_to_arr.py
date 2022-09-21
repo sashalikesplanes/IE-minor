74bd79c48dfef21be93048ee2d6486640e2d3ac5
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-09-21 16:06:19 +0200

    made more eyes

--- scripts/img_to_arr.py
+++ scripts/img_to_arr.py
@@ -5,8 +5,18 @@ import imageio.v3 as iio
 import sys
 
 im = iio.imread(sys.argv[1] + '.png')
-im = np.reshape(im[:, :, 0:3], (256, 3))
-pixel_list = [tuple(pixel.tolist()) for pixel in im]
+im_no_transparency = im[:, :, 0:3]
+
+# Flip every other row to allign with pattern of LEDs
+im_flipped = []
+for i, row in enumerate(im_no_transparency):
+    if i % 2 == 0:
+        im_flipped.append(row[::-1])
+    else:
+        im_flipped.append(row)
+    
+im_reshaped = np.reshape(np.array(im_flipped), (256, 3))
+pixel_list = [tuple(pixel.tolist()) for pixel in im_reshaped]
 
 with open(sys.argv[1] + '.json', 'w') as f:
     json.dump(pixel_list, f)
