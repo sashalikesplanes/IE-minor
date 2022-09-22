094a4fd21a13cd2577c5694cbc89ac24364bf1be
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-09-22 14:22:54 +0200

    created all eye animations

--- scripts/img_to_arr.py
+++ scripts/img_to_arr.py
@@ -3,23 +3,31 @@ import json
 import numpy as np
 import imageio.v3 as iio
 import sys
+import os
 
-im = iio.imread(sys.argv[1] + '.png')
-im_no_transparency = im[:, :, 0:3]
+image_files = os.listdir()
+
+for file in image_files:
+    if file[0:3] != 'eye':
+        continue
+    im = iio.imread(file)
+    im_no_transparency = im[:, :, 0:3]
 
 # Flip every other row to allign with pattern of LEDs
-im_flipped = []
-for i, row in enumerate(im_no_transparency):
-    if i % 2 == 0:
-        im_flipped.append(row[::-1])
-    else:
-        im_flipped.append(row)
-    
-im_reshaped = np.reshape(np.array(im_flipped), (256, 3))
-pixel_list = [tuple(pixel.tolist()) for pixel in im_reshaped]
+    im_flipped = []
+    for i, row in enumerate(im_no_transparency):
+        if i % 2 == 0:
+            im_flipped.append(row[::-1])
+        else:
+            im_flipped.append(row)
 
-with open(sys.argv[1] + '.json', 'w') as f:
-    json.dump(pixel_list, f)
+    im_reshaped = np.reshape(np.array(im_flipped), (256, 3))
+    pixel_list = [tuple(pixel.tolist()) for pixel in im_reshaped]
 
-print('done converting: ' + sys.argv[1])
+    with open(file.split('.')[0] + '.json', 'w') as f:
+        json.dump(pixel_list, f)
+
+    print('done converting: ' + file)
+
+
 
