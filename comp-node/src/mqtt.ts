b4bee56857db86f008dd9d63d7dba18633253258
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2023-01-16 08:12:03 +0100

    feat: create image observable

--- comp-node/src/mqtt.ts
+++ comp-node/src/mqtt.ts
@@ -1,4 +1,5 @@
-import { connect } from "mqtt";
+import { connect, OnMessageCallback } from "mqtt";
+import { bindCallback, filter, map, tap } from "rxjs";
 import { ImageChunk } from "./types";
 const TOPIC_CAM_0 = "unitv2-cam0";
 const TOPIC_CAM_1 = "unitv2-cam1";
@@ -11,32 +12,40 @@ client.on("connect", () => {
 	client.subscribe(TOPIC_CAM_1);
 });
 
-let imageChunks: ImageChunk[] = [];
-client.on("message", async (topic, message) => {
-	if (topic === TOPIC_CAM_0 || topic === TOPIC_CAM_1) {
-		imageChunks.push(JSON.parse(message.toString()) as ImageChunk);
-		if (imageChunks.length < imageChunks[0].total_chunks) {
-			return;
-		}
+const message$ = bindCallback(client.on)("message")
+export const imageMessage$ = message$.pipe(
+	tap(v => console.log(v)),
+	map(handleMessage),
+)
 
-		const imageBinaryString = imageChunks.reduce((acc, chunk) => {
-			if (chunk.frame_i !== imageChunks[0].frame_i) {
-				throw new Error("Frame index mismatch");
+
+function handleMessage(): OnMessageCallback {
+	let imageChunks: ImageChunk[] = [];
+	return async (topic, message) => {
+		if (topic === TOPIC_CAM_0 || topic === TOPIC_CAM_1) {
+			imageChunks.push(JSON.parse(message.toString()) as ImageChunk);
+			if (imageChunks.length < imageChunks[0].total_chunks) {
+				return;
 			}
-			return acc + chunk.chunk;
-		}, "");
 
-		const imageBuffer = Buffer.from(imageBinaryString, "base64");
-		console.log('got image buffer')
-    
-    // Now we get the buffer onto the heap
-    // buffer is free to have next image loaded
-    // we transform the buffer to paint the detections on it
-    // for now lets just save the image which results
-		imageChunks = [];
-	}
-});
+			const imageBinaryString = imageChunks.reduce((acc, chunk) => {
+				if (chunk.frame_i !== imageChunks[0].frame_i) {
+					throw new Error("Frame index mismatch");
+				}
+				return acc + chunk.chunk;
+			}, "");
 
+			const imageBuffer = Buffer.from(imageBinaryString, "base64");
+			console.log('got image buffer');
+
+			// Now we get the buffer onto the heap
+			// buffer is free to have next image loaded
+			// we transform the buffer to paint the detections on it
+			// for now lets just save the image which results
+			imageChunks = [];
+		}
+	};
+}
 
 export function messageStream() {
 
