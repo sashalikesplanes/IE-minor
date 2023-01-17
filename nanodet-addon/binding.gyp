dfb0944eecae976edad11cc12cd57d80c2d2e4f3
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2023-01-17 21:48:10 +0100

    compiled the model

--- nanodet-addon/binding.gyp
+++ nanodet-addon/binding.gyp
@@ -2,16 +2,28 @@
   'targets': [
     {
       'target_name': 'nanodet-native',
-      'sources': [ 'src/nanodet.cc' ],
-      'include_dirs': ["<!@(node -p \"require('node-addon-api').include\")"],
+      'sources': [ './src/main.cc', './src/nanodet.h', './src/nanodet.cc' ],
+      'include_dirs': [
+        "<!@(node -p \"require('node-addon-api').include\")",
+        '.',
+        '/usr/local/lib',
+        '/usr/local/include/opencv4',
+        '/Users/sasha/Documents/code/repos/ncnn/build/install/include/ncnn'
+      ],
+       "library_dirs": [ "/usr/local/lib"             ],
       'dependencies': ["<!(node -p \"require('node-addon-api').gyp\")"],
       'cflags!': [ '-fno-exceptions' ],
       'cflags_cc!': [ '-fno-exceptions' ],
       'xcode_settings': {
         'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
         'CLANG_CXX_LIBRARY': 'libc++',
-        'MACOSX_DEPLOYMENT_TARGET': '10.7',
+        'MACOSX_DEPLOYMENT_TARGET': '11.7',
         'GCC_SYMBOLS_PRIVATE_EXTERN': 'YES' # -fvisibility=hidden
+      },
+      'link_settings': {
+        'libraries': [
+            '/usr/local/lib/libopencv_core.dylib'
+        ],
       },
       'msvs_settings': {
         'VCCLCompilerTool': { 'ExceptionHandling': 1 },
--- nanodet-addon/src/nanodet.cc
+++ nanodet-addon/src/nanodet.cc
@@ -1,16 +1,525 @@
+//
+// Create by RangiLyu
+// 2020 / 10 / 2
+//
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "nanodet.h"
 #include <napi.h>
+// #include <iostream>
 
-using namespace Napi;
+struct object_rect {
+    int x;
+    int y;
+    int width;
+    int height;
+};
 
-Napi::String Method(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  return Napi::String::New(env, "world");
+
+const int color_list[80][3] =
+{
+    //{255 ,255 ,255}, //bg
+    {216 , 82 , 24},
+    {236 ,176 , 31},
+    {125 , 46 ,141},
+    {118 ,171 , 47},
+    { 76 ,189 ,237},
+    {238 , 19 , 46},
+    { 76 , 76 , 76},
+    {153 ,153 ,153},
+    {255 ,  0 ,  0},
+    {255 ,127 ,  0},
+    {190 ,190 ,  0},
+    {  0 ,255 ,  0},
+    {  0 ,  0 ,255},
+    {170 ,  0 ,255},
+    { 84 , 84 ,  0},
+    { 84 ,170 ,  0},
+    { 84 ,255 ,  0},
+    {170 , 84 ,  0},
+    {170 ,170 ,  0},
+    {170 ,255 ,  0},
+    {255 , 84 ,  0},
+    {255 ,170 ,  0},
+    {255 ,255 ,  0},
+    {  0 , 84 ,127},
+    {  0 ,170 ,127},
+    {  0 ,255 ,127},
+    { 84 ,  0 ,127},
+    { 84 , 84 ,127},
+    { 84 ,170 ,127},
+    { 84 ,255 ,127},
+    {170 ,  0 ,127},
+    {170 , 84 ,127},
+    {170 ,170 ,127},
+    {170 ,255 ,127},
+    {255 ,  0 ,127},
+    {255 , 84 ,127},
+    {255 ,170 ,127},
+    {255 ,255 ,127},
+    {  0 , 84 ,255},
+    {  0 ,170 ,255},
+    {  0 ,255 ,255},
+    { 84 ,  0 ,255},
+    { 84 , 84 ,255},
+    { 84 ,170 ,255},
+    { 84 ,255 ,255},
+    {170 ,  0 ,255},
+    {170 , 84 ,255},
+    {170 ,170 ,255},
+    {170 ,255 ,255},
+    {255 ,  0 ,255},
+    {255 , 84 ,255},
+    {255 ,170 ,255},
+    { 42 ,  0 ,  0},
+    { 84 ,  0 ,  0},
+    {127 ,  0 ,  0},
+    {170 ,  0 ,  0},
+    {212 ,  0 ,  0},
+    {255 ,  0 ,  0},
+    {  0 , 42 ,  0},
+    {  0 , 84 ,  0},
+    {  0 ,127 ,  0},
+    {  0 ,170 ,  0},
+    {  0 ,212 ,  0},
+    {  0 ,255 ,  0},
+    {  0 ,  0 , 42},
+    {  0 ,  0 , 84},
+    {  0 ,  0 ,127},
+    {  0 ,  0 ,170},
+    {  0 ,  0 ,212},
+    {  0 ,  0 ,255},
+    {  0 ,  0 ,  0},
+    { 36 , 36 , 36},
+    { 72 , 72 , 72},
+    {109 ,109 ,109},
+    {145 ,145 ,145},
+    {182 ,182 ,182},
+    {218 ,218 ,218},
+    {  0 ,113 ,188},
+    { 80 ,182 ,188},
+    {127 ,127 ,  0},
+};
+
+void draw_bboxes(const cv::Mat& bgr, const std::vector<BoxInfo>& bboxes, object_rect effect_roi)
+{
+    static const char* class_names[] = { "person", "bicycle", "car", "motorcycle", "airplane", "bus",
+                                        "train", "truck", "boat", "traffic light", "fire hydrant",
+                                        "stop sign", "parking meter", "bench", "bird", "cat", "dog",
+                                        "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+                                        "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+                                        "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                                        "baseball glove", "skateboard", "surfboard", "tennis racket",
+                                        "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl",
+                                        "banana", "apple", "sandwich", "orange", "broccoli", "carrot",
+                                        "hot dog", "pizza", "donut", "cake", "chair", "couch",
+                                        "potted plant", "bed", "dining table", "toilet", "tv", "laptop",
+                                        "mouse", "remote", "keyboard", "cell phone", "microwave", "oven",
+                                        "toaster", "sink", "refrigerator", "book", "clock", "vase",
+                                        "scissors", "teddy bear", "hair drier", "toothbrush"
+    };
+
+    cv::Mat image = bgr.clone();
+    int src_w = image.cols;
+    int src_h = image.rows;
+    int dst_w = effect_roi.width;
+    int dst_h = effect_roi.height;
+    float width_ratio = (float)src_w / (float)dst_w;
+    float height_ratio = (float)src_h / (float)dst_h;
+
+
+    for (size_t i = 0; i < bboxes.size(); i++)
+    {
+        const BoxInfo& bbox = bboxes[i];
+        cv::Scalar color = cv::Scalar(color_list[bbox.label][0], color_list[bbox.label][1], color_list[bbox.label][2]);
+        //fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f %.2f\n", bbox.label, bbox.score,
+        //    bbox.x1, bbox.y1, bbox.x2, bbox.y2);
+
+        cv::rectangle(image, cv::Rect(cv::Point((bbox.x1 - effect_roi.x) * width_ratio, (bbox.y1 - effect_roi.y) * height_ratio),
+                                      cv::Point((bbox.x2 - effect_roi.x) * width_ratio, (bbox.y2 - effect_roi.y) * height_ratio)), color);
+
+        char text[256];
+        sprintf(text, "%s %.1f%%", class_names[bbox.label], bbox.score * 100);
+
+        int baseLine = 0;
+        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.4, 1, &baseLine);
+
+        int x = (bbox.x1 - effect_roi.x) * width_ratio;
+        int y = (bbox.y1 - effect_roi.y) * height_ratio - label_size.height - baseLine;
+        if (y < 0)
+            y = 0;
+        if (x + label_size.width > image.cols)
+            x = image.cols - label_size.width;
+
+        cv::rectangle(image, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+            color, -1);
+
+        cv::putText(image, text, cv::Point(x, y + label_size.height),
+            cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(255, 255, 255));
+    }
+
+    cv::imshow("image", image);
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "Nanodet"),
-              Napi::Function::New(env, Method));
+inline float fast_exp(float x)
+{
+    union {
+        uint32_t i;
+        float f;
+    } v{};
+    v.i = (1 << 23) * (1.4426950409 * x + 126.93490512f);
+    return v.f;
+}
+
+inline float sigmoid(float x)
+{
+    return 1.0f / (1.0f + fast_exp(-x));
+}
+
+int resize_uniform(cv::Mat& src, cv::Mat& dst, cv::Size dst_size, object_rect& effect_area)
+{
+    int w = src.cols;
+    int h = src.rows;
+    int dst_w = dst_size.width;
+    int dst_h = dst_size.height;
+    //std::cout << "src: (" << h << ", " << w << ")" << std::endl;
+    dst = cv::Mat(cv::Size(dst_w, dst_h), CV_8UC3, cv::Scalar(0));
+
+    float ratio_src = w * 1.0 / h;
+    float ratio_dst = dst_w * 1.0 / dst_h;
+
+    int tmp_w = 0;
+    int tmp_h = 0;
+    if (ratio_src > ratio_dst) {
+        tmp_w = dst_w;
+        tmp_h = floor((dst_w * 1.0 / w) * h);
+    }
+    else if (ratio_src < ratio_dst) {
+        tmp_h = dst_h;
+        tmp_w = floor((dst_h * 1.0 / h) * w);
+    }
+    else {
+        cv::resize(src, dst, dst_size);
+        effect_area.x = 0;
+        effect_area.y = 0;
+        effect_area.width = dst_w;
+        effect_area.height = dst_h;
+        return 0;
+    }
+
+    //std::cout << "tmp: (" << tmp_h << ", " << tmp_w << ")" << std::endl;
+    cv::Mat tmp;
+    cv::resize(src, tmp, cv::Size(tmp_w, tmp_h));
+
+    if (tmp_w != dst_w) {
+        int index_w = floor((dst_w - tmp_w) / 2.0);
+        //std::cout << "index_w: " << index_w << std::endl;
+        for (int i = 0; i < dst_h; i++) {
+            memcpy(dst.data + i * dst_w * 3 + index_w * 3, tmp.data + i * tmp_w * 3, tmp_w * 3);
+        }
+        effect_area.x = index_w;
+        effect_area.y = 0;
+        effect_area.width = tmp_w;
+        effect_area.height = tmp_h;
+    }
+    else if (tmp_h != dst_h) {
+        int index_h = floor((dst_h - tmp_h) / 2.0);
+        //std::cout << "index_h: " << index_h << std::endl;
+        memcpy(dst.data + index_h * dst_w * 3, tmp.data, tmp_w * tmp_h * 3);
+        effect_area.x = 0;
+        effect_area.y = index_h;
+        effect_area.width = tmp_w;
+        effect_area.height = tmp_h;
+    }
+    else {
+        printf("error\n");
+    }
+    //cv::imshow("dst", dst);
+    //cv::waitKey(0);
+    return 0;
+}
+
+template<typename _Tp>
+int activation_function_softmax(const _Tp* src, _Tp* dst, int length)
+{
+    const _Tp alpha = *std::max_element(src, src + length);
+    _Tp denominator{ 0 };
+
+    for (int i = 0; i < length; ++i) {
+        dst[i] = fast_exp(src[i] - alpha);
+        denominator += dst[i];
+    }
+
+    for (int i = 0; i < length; ++i) {
+        dst[i] /= denominator;
+    }
+
+    return 0;
+}
+
+
+static void generate_grid_center_priors(const int input_height, const int input_width, std::vector<int>& strides, std::vector<CenterPrior>& center_priors)
+{
+    for (int i = 0; i < (int)strides.size(); i++)
+    {
+        int stride = strides[i];
+        int feat_w = ceil((float)input_width / stride);
+        int feat_h = ceil((float)input_height / stride);
+        for (int y = 0; y < feat_h; y++)
+        {
+            for (int x = 0; x < feat_w; x++)
+            {
+                CenterPrior ct;
+                ct.x = x;
+                ct.y = y;
+                ct.stride = stride;
+                center_priors.push_back(ct);
+            }
+        }
+    }
+}
+
+Napi::Object NanoDet::Init(Napi::Env env, Napi::Object exports) {
+  Napi::Function func = DefineClass(env, "NanoDet", {InstanceMethod("detectFromImage", &NanoDet::DetectFromImage)});
+
+  exports.Set("NanoDet", func);
   return exports;
 }
 
-NODE_API_MODULE(addon, Init)
+bool NanoDet::hasGPU = false;
+NanoDet* NanoDet::detector = nullptr;
+
+NanoDet::NanoDet(const Napi::CallbackInfo& info)
+    : Napi::ObjectWrap<NanoDet>(info) {
+    Napi::Env env = info.Env();
+    // We need the following from Napi
+    int length = info.Length();
+    if (length < 4 || !info[0].IsString() || !info[1].IsString() || !info[2].IsBoolean() || !info[3].IsBoolean()) {
+        Napi::TypeError::New(env, "String, String, Boolean, Boolean expected").ThrowAsJavaScriptException();
+        return;
+    }
+    
+    std::string param_ = info[0].As<Napi::String>().Utf8Value();
+    std::string bin_ = info[1].As<Napi::String>().Utf8Value();
+    Napi::Boolean useGPU_ = info[2].As<Napi::Boolean>();
+    Napi::Boolean showImage_ = info[3].As<Napi::Boolean>();
+
+    char* param = (char*)param_.c_str();
+    char* bin = (char*)bin_.c_str();
+    bool useGPU = bool(useGPU_);
+    bool showImage = bool(showImage_);
+
+    this->Net = new ncnn::Net();
+    // opt
+#if NCNN_VULKAN
+    this->hasGPU = ncnn::get_gpu_count() > 0;
+#endif
+    this->showImage = showImage;
+    this->Net->opt.use_vulkan_compute = this->hasGPU && useGPU;
+    this->Net->opt.use_fp16_arithmetic = true;
+    this->Net->load_param(param);
+    this->Net->load_model(bin);
+}
+
+NanoDet::~NanoDet()
+{
+    delete this->Net;
+}
+
+void NanoDet::preprocess(cv::Mat& image, ncnn::Mat& in)
+{
+    int img_w = image.cols;
+    int img_h = image.rows;
+
+    in = ncnn::Mat::from_pixels(image.data, ncnn::Mat::PIXEL_BGR, img_w, img_h);
+    //in = ncnn::Mat::from_pixels_resize(image.data, ncnn::Mat::PIXEL_BGR, img_w, img_h, this->input_width, this->input_height);
+
+    const float mean_vals[3] = { 103.53f, 116.28f, 123.675f };
+    const float norm_vals[3] = { 0.017429f, 0.017507f, 0.017125f };
+    in.substract_mean_normalize(mean_vals, norm_vals);
+}
+
+std::vector<BoxInfo> NanoDet::detect(cv::Mat image, float score_threshold, float nms_threshold)
+{
+    ncnn::Mat input;
+    preprocess(image, input);
+
+    //double start = ncnn::get_current_time();
+
+    auto ex = this->Net->create_extractor();
+    ex.set_light_mode(false);
+    ex.set_num_threads(4);
+#if NCNN_VULKAN
+    ex.set_vulkan_compute(this->hasGPU);
+#endif
+    ex.input("data", input);
+
+    std::vector<std::vector<BoxInfo>> results;
+    results.resize(this->num_class);
+
+    ncnn::Mat out;
+    ex.extract("output", out);
+    // printf("%d %d %d \n", out.w, out.h, out.c);
+
+    // generate center priors in format of (x, y, stride)
+    std::vector<CenterPrior> center_priors;
+    generate_grid_center_priors(this->input_size[0], this->input_size[1], this->strides, center_priors);
+
+    this->decode_infer(out, center_priors, score_threshold, results);
+
+    std::vector<BoxInfo> dets;
+    for (int i = 0; i < (int)results.size(); i++)
+    {
+        this->nms(results[i], nms_threshold);
+
+        for (auto box : results[i])
+        {
+            dets.push_back(box);
+        }
+    }
+
+    //double end = ncnn::get_current_time();
+    //double time = end - start;
+    //printf("Detect Time:%7.2f \n", time);
+
+    return dets;
+}
+
+void NanoDet::decode_infer(ncnn::Mat& feats, std::vector<CenterPrior>& center_priors, float threshold, std::vector<std::vector<BoxInfo>>& results)
+{
+    const int num_points = center_priors.size();
+    //printf("num_points:%d\n", num_points);
+
+    //cv::Mat debug_heatmap = cv::Mat(feature_h, feature_w, CV_8UC3);
+    for (int idx = 0; idx < num_points; idx++)
+    {
+        const int ct_x = center_priors[idx].x;
+        const int ct_y = center_priors[idx].y;
+        const int stride = center_priors[idx].stride;
+
+        const float* scores = feats.row(idx);
+        float score = 0;
+        int cur_label = 0;
+        for (int label = 0; label < this->num_class; label++)
+        {
+            if (scores[label] > score)
+            {
+                score = scores[label];
+                cur_label = label;
+            }
+        }
+        if (score > threshold)
+        {
+            //std::cout << "label:" << cur_label << " score:" << score << std::endl;
+            const float* bbox_pred = feats.row(idx) + this->num_class;
+            results[cur_label].push_back(this->disPred2Bbox(bbox_pred, cur_label, score, ct_x, ct_y, stride));
+            //debug_heatmap.at<cv::Vec3b>(row, col)[0] = 255;
+            //cv::imshow("debug", debug_heatmap);
+        }
+    }
+}
+
+BoxInfo NanoDet::disPred2Bbox(const float*& dfl_det, int label, float score, int x, int y, int stride)
+{
+    float ct_x = x * stride;
+    float ct_y = y * stride;
+    std::vector<float> dis_pred;
+    dis_pred.resize(4);
+    for (int i = 0; i < 4; i++)
+    {
+        float dis = 0;
+        float* dis_after_sm = new float[this->reg_max + 1];
+        activation_function_softmax(dfl_det + i * (this->reg_max + 1), dis_after_sm, this->reg_max + 1);
+        for (int j = 0; j < this->reg_max + 1; j++)
+        {
+            dis += j * dis_after_sm[j];
+        }
+        dis *= stride;
+        //std::cout << "dis:" << dis << std::endl;
+        dis_pred[i] = dis;
+        delete[] dis_after_sm;
+    }
+    float xmin = (std::max)(ct_x - dis_pred[0], .0f);
+    float ymin = (std::max)(ct_y - dis_pred[1], .0f);
+    float xmax = (std::min)(ct_x + dis_pred[2], (float)this->input_size[0]);
+    float ymax = (std::min)(ct_y + dis_pred[3], (float)this->input_size[1]);
+
+    //std::cout << xmin << "," << ymin << "," << xmax << "," << xmax << "," << std::endl;
+    return BoxInfo { xmin, ymin, xmax, ymax, score, label };
+}
+
+void NanoDet::nms(std::vector<BoxInfo>& input_boxes, float NMS_THRESH)
+{
+    std::sort(input_boxes.begin(), input_boxes.end(), [](BoxInfo a, BoxInfo b) { return a.score > b.score; });
+    std::vector<float> vArea(input_boxes.size());
+    for (int i = 0; i < int(input_boxes.size()); ++i) {
+        vArea[i] = (input_boxes.at(i).x2 - input_boxes.at(i).x1 + 1)
+            * (input_boxes.at(i).y2 - input_boxes.at(i).y1 + 1);
+    }
+    for (int i = 0; i < int(input_boxes.size()); ++i) {
+        for (int j = i + 1; j < int(input_boxes.size());) {
+            float xx1 = (std::max)(input_boxes[i].x1, input_boxes[j].x1);
+            float yy1 = (std::max)(input_boxes[i].y1, input_boxes[j].y1);
+            float xx2 = (std::min)(input_boxes[i].x2, input_boxes[j].x2);
+            float yy2 = (std::min)(input_boxes[i].y2, input_boxes[j].y2);
+            float w = (std::max)(float(0), xx2 - xx1 + 1);
+            float h = (std::max)(float(0), yy2 - yy1 + 1);
+            float inter = w * h;
+            float ovr = inter / (vArea[i] + vArea[j] - inter);
+            if (ovr >= NMS_THRESH) {
+                input_boxes.erase(input_boxes.begin() + j);
+                vArea.erase(vArea.begin() + j);
+            }
+            else {
+                j++;
+            }
+        }
+    }
+}
+
+Napi::Value NanoDet::DetectFromImage(const Napi::CallbackInfo& info)
+{
+    if (info.Length() <= 0 || !info[0].IsString())
+    {
+        Napi::TypeError::New(info.Env(), "String expected").ThrowAsJavaScriptException();
+        return Napi::String::New(info.Env(), "failed");
+    }
+    
+    std::string image_path = std::string(info[0].As<Napi::String>());
+
+    int height = this->input_size[0];
+    int width = this->input_size[1];
+    
+    cv::Mat image = cv::imread(image_path);
+    if (image.empty())
+    {
+        fprintf(stderr, "cv::imread %s failed\n", image_path.c_str());
+        return Napi::String::New(info.Env(), "failed");
+    }
+    object_rect effect_roi;
+    cv::Mat resized_img;
+    resize_uniform(image, resized_img, cv::Size(width, height), effect_roi);
+    auto results = this->detect(resized_img, 0.4, 0.5);
+    // now we want to convert results to json
+    std::string json = "[";
+    for (int i = 0; i < results.size(); i++)
+    {
+        json += "{";
+        json += "\"label\":" + std::to_string(results[i].label) + ",";
+        json += "\"score\":" + std::to_string(results[i].score) + ",";
+        json += "\"x1\":" + std::to_string(results[i].x1) + ",";
+        json += "\"y1\":" + std::to_string(results[i].y1) + ",";
+        json += "\"x2\":" + std::to_string(results[i].x2) + ",";
+        json += "\"y2\":" + std::to_string(results[i].y2);
+        json += "},";
+    }
+    json += "]";
+    
+    if (this->showImage)
+    {
+        draw_bboxes(image, results, effect_roi);
+        cv::waitKey(0); // Do we need this?
+    }
+    return Napi::String::New(info.Env(), json);
+}
